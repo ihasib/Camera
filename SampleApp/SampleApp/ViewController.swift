89baafdec2bd8cbd36ea75abbe79875706cca784
Hasib <hasib.jhd@gmail.com>
2023-07-20 01:57:17 +0600

    CameraKit and Sample app integrated in single workspace

---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by S. M. Hasibur Rahman on 20/7/23.
+//
+
+import UIKit
+import CameraKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
